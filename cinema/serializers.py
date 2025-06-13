f38d1cde91625af182bbeb4deb88b13990a572c2
Aleksander Rokita <kelor2@o2.pl>
2025-06-13 13:37:58 +0200

    Fix code formatting

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -82,7 +82,9 @@ class MovieSessionSerializer(BaseMovieSessionSerializer):
 
 class MovieSessionListSerializer(BaseMovieSessionSerializer):
     movie_title = serializers.CharField(source="movie.title", read_only=True)
-    cinema_hall_name = serializers.CharField(source="cinema_hall.name", read_only=True)
+    cinema_hall_name = serializers.CharField(
+        source="cinema_hall.name", read_only=True
+    )
     cinema_hall_capacity = serializers.IntegerField(
         source="cinema_hall.capacity", read_only=True
     )
--- cinema/urls.py
+++ cinema/urls.py
@@ -14,7 +14,11 @@ router.register("genres", GenreViewSet, basename="genres")
 router.register("actors", ActorViewSet, basename="actors")
 router.register("cinema_halls", CinemaHallViewSet, basename="cinema_halls")
 router.register("movies", MovieViewSet, basename="movies")
-router.register("movie_sessions", MovieSessionViewSet, basename="movie_sessions")
+router.register(
+    "movie_sessions",
+    MovieSessionViewSet,
+    basename="movie_sessions"
+)
 
 urlpatterns = [
     path("", include(router.urls)),
